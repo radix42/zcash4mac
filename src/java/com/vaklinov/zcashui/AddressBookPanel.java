a73b763cf9db51d6e1515dbbd42ac0c5957dde84
Zlatin Balevsky <zlatinb@gmail.com>
2016-12-06 18:52:26 +0000

    work on the table

--- src/java/com/vaklinov/zcashui/AddressBookPanel.java
+++ src/java/com/vaklinov/zcashui/AddressBookPanel.java
@@ -1,23 +1,42 @@
 package com.vaklinov.zcashui;
 
 import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.event.TableModelListener;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableColumnModel;
 import javax.swing.table.TableColumn;
-import javax.swing.table.TableColumnModel;
-import javax.swing.table.TableModel;
 
 public class AddressBookPanel extends JPanel {
 
+    private static class AddressBookEntry {
+        final String name,address;
+        AddressBookEntry(String name, String address) {
+            this.name = name;
+            this.address = address;
+        }
+    }
+    
+    private final List<AddressBookEntry> entries =
+            new ArrayList<>();
 
+    private final Set<String> names = new HashSet<>();
+    
+    private JTable table;
+    
     private JPanel buildButtonsPanel() {
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEtchedBorder());
@@ -25,12 +44,13 @@ public class AddressBookPanel extends JPanel {
     }
 
     private JScrollPane buildTablePanel() {
-//        JTable table = new AddressBookTable();
-        JTable table = new JTable(new AddressBookTableModel(),new DefaultTableColumnModel());
+        table = new JTable(new AddressBookTableModel(),new DefaultTableColumnModel());
         TableColumn nameColumn = new TableColumn(0);
         TableColumn addressColumn = new TableColumn(1);
         table.addColumn(nameColumn);
         table.addColumn(addressColumn);
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); // one at a time
+        table.addMouseListener(new AddressMouseListener());
         JScrollPane scrollPane = new JScrollPane(table);
         return scrollPane;
     }
@@ -40,82 +60,78 @@ public class AddressBookPanel extends JPanel {
         setLayout(boxLayout);
         add(buildTablePanel());
         add(buildButtonsPanel());
-    }
-}
-
-class AddressBookTable extends DataTable {
-    AddressBookTable () {
-        super(new String[0][0],new String[]{"Name","Address"});
-        setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
-    }
-}
-
-class AddressBookEntry {
-    String name,address;
-}
-
-class AddressBookTableModel implements TableModel {
-
-    private final List<AddressBookEntry> entries =
-            new ArrayList<>();
-    
-    AddressBookTableModel() {
-        AddressBookEntry entry = new AddressBookEntry();
-        entry.name = "pesho";
-        entry.address = "asdf";
-        entries.add(entry);
-    }
-    @Override
-    public int getRowCount() {
-        return entries.size();
+        
+        // add some entries for testing
+        AddressBookEntry entry1 = new AddressBookEntry("pesho","asdf");
+        AddressBookEntry entry2 = new AddressBookEntry("gosho","fdsa");
+        AddressBookEntry entry3 = new AddressBookEntry("tosho","qwer");
+        entries.add(entry1);entries.add(entry2);entries.add(entry3);
     }
 
-    @Override
-    public int getColumnCount() {
-        return 2;
-    }
+    private class AddressMouseListener extends MouseAdapter {
 
-    @Override
-    public String getColumnName(int columnIndex) {
-        switch(columnIndex) {
-        case 0 : return "name";
-        case 1 : return "address";
-        default:
-            throw new IllegalArgumentException("invalid column "+columnIndex);
+        @Override
+        public void mousePressed(MouseEvent e) {
+            if (e.isConsumed() || (!e.isPopupTrigger()))
+                return;
+
+            int row = table.rowAtPoint(e.getPoint());
+            int column = table.columnAtPoint(e.getPoint());
+            table.changeSelection(row, column, false, false);
+            AddressBookEntry entry = entries.get(row);
+            JPopupMenu menu = new JPopupMenu();
+            JMenuItem sendCash = new JMenuItem("Send ZCash to "+entry.name);
+            menu.add(sendCash);
+            JMenuItem copyAddress = new JMenuItem("Copy address to clipboard");
+            menu.add(copyAddress);
+            JMenuItem deleteEntry = new JMenuItem("Delete "+entry.name+" from contacts");
+            menu.add(deleteEntry);
+            menu.show(e.getComponent(), e.getPoint().x, e.getPoint().y);
+            e.consume();
         }
     }
 
-    @Override
-    public Class<?> getColumnClass(int columnIndex) {
-        return String.class;
-    }
+    class AddressBookTableModel extends AbstractTableModel {
 
-    @Override
-    public boolean isCellEditable(int rowIndex, int columnIndex) {
-        return false;
-    }
-
-    @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        AddressBookEntry entry = entries.get(rowIndex);
-        switch(columnIndex) {
-        case 0 : return entry.name;
-        case 1 : return entry.address;
-        default:
-            throw new IllegalArgumentException("bad column "+columnIndex);
+        @Override
+        public int getRowCount() {
+            return entries.size();
         }
-    }
 
-    @Override
-    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-        throw new UnsupportedOperationException();
-    }
+        @Override
+        public int getColumnCount() {
+            return 2;
+        }
 
-    @Override
-    public void addTableModelListener(TableModelListener l) {
-    }
+        @Override
+        public String getColumnName(int columnIndex) {
+            switch(columnIndex) {
+            case 0 : return "name";
+            case 1 : return "address";
+            default:
+                throw new IllegalArgumentException("invalid column "+columnIndex);
+            }
+        }
 
-    @Override
-    public void removeTableModelListener(TableModelListener l) {
+        @Override
+        public Class<?> getColumnClass(int columnIndex) {
+            return String.class;
+        }
+
+        @Override
+        public boolean isCellEditable(int rowIndex, int columnIndex) {
+            return false;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            AddressBookEntry entry = entries.get(rowIndex);
+            switch(columnIndex) {
+            case 0 : return entry.name;
+            case 1 : return entry.address;
+            default:
+                throw new IllegalArgumentException("bad column "+columnIndex);
+            }
+        }
     }
 }
