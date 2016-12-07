a2fe76c7796a2591f0181520cdb4b5ffad591834
Zlatin Balevsky <zlatinb@gmail.com>
2016-12-07 19:09:36 +0000

    launch first-run.sh and fetch-params.sh

--- src/java/com/vaklinov/zcashui/StartupProgressDialog.java
+++ src/java/com/vaklinov/zcashui/StartupProgressDialog.java
@@ -2,9 +2,11 @@ package com.vaklinov.zcashui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.util.Map;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -15,6 +17,7 @@ import javax.swing.SwingUtilities;
 
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
+import com.vaklinov.zcashui.OSUtil.OS_TYPE;
 import com.vaklinov.zcashui.ZCashClientCaller.WalletCallException;
 
 public class StartupProgressDialog extends JWindow {
@@ -53,6 +56,12 @@ public class StartupProgressDialog extends JWindow {
     
     public void waitForStartup() throws IOException,
         InterruptedException,WalletCallException,InvocationTargetException {
+        
+        // special handling of OSX app bundle
+        if (OSUtil.getOSType() == OS_TYPE.MAC_OS && 
+                "true".equalsIgnoreCase(System.getProperty("launching.from.appbundle")))
+            performOSXBundleLaunch();
+        
         System.out.println("trying to start zcashd");
         final Process daemonProcess = clientCaller.startDaemon();
         while(true) {
@@ -88,5 +97,31 @@ public class StartupProgressDialog extends JWindow {
                     System.out.println("not stopping zcashd");
             }
         });
+    }
+    
+    private void performOSXBundleLaunch() throws IOException, InterruptedException {
+        
+        File bundlePath = new File(System.getProperty("zcash.location.dir"));
+        bundlePath = bundlePath.getCanonicalFile();
+        
+        // run "first-run.sh"
+        File firstRun = new File(bundlePath,"first-run.sh");
+        Process firstRunProcess = Runtime.getRuntime().exec(firstRun.getCanonicalPath());
+        firstRunProcess.waitFor();
+        
+        // then run fetch-params.sh
+        File fetchParams = new File(bundlePath,"fetch-params.sh");
+        ProcessBuilder pb = new ProcessBuilder(fetchParams.getCanonicalPath());
+        Map<String, String> env = pb.environment();
+        String path = env.get("PATH");
+        path = path + ":"+bundlePath.getCanonicalPath();
+        env.put("PATH", path);
+        Process fetchParamsProcess = pb.start();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                progressLabel.setText("Fetching parameters...");
+            }
+        });
+        fetchParamsProcess.waitFor();
     }
 }
