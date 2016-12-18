a028ab892f8ad3294cd0d1df2fdabb35525364b3
David Mercer <radix42@gmail.com>
2016-12-18 11:28:21 -0700

    license text and related changes

--- macosx/first-run.sh
+++ macosx/first-run.sh
@@ -12,3 +12,9 @@ if [ ! -f "${CONF}" ]; then
     echo "showmetrics=0" >> "${CONF}"
     echo "gen=0" >> "${CONF}"
 fi
+
+ADDR=~/Library/Application\ Support/zcash4mac/addressBook.csv
+if [ ! -f "${ADDR}" ]; then
+    echo "t1WjQ6yqv8Yh9pcc2f4JMrMpVJ3LNQXFt3u,zcash4mac donations" >> "${ADDR}"
+    echo "t1VAggo7RusLVBzHSeYbGkxDQQhLZyigxty,ZCashSwingUI donations" >> "${ADDR}"
+fi
